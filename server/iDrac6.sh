c8cf3c3325e98c0b480e7dfe5e32002c383672a8
Peter Rose <peterrose1994@gmail.com>
2020-05-03 14:09:08 -0400

    Minor script update

--- server/iDrac6.sh
+++ server/iDrac6.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker run -d -v /media/bigdata/Archive:/vmedia -p 5800:5800 -p 5900:5900 -e IDRAC_HOST=10.0.0.52 -e IDRAC_USER=root -e IDRAC_PASSWORD=calvin domistyle/idrac6
 
+# Windows Equivalent
+# docker run -d -v D:/Archive/ISO:/vmedia -p 5800:5800 -p 5900:5900 -e IDRAC_HOST=10.0.0.52 -e IDRAC_USER=root -e IDRAC_PASSWORD=calvin domistyle/idrac6
